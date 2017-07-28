ce560d9bad267aec829b0d8ad4ae07d2bf49fd46
Jarvis Prestidge <jarvis@truelayer.com>
2017-07-28 12:53:45 +0100

    add and populate championInfo data type

--- championselect.go
+++ championselect.go
@@ -2,56 +2,77 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"fmt"
 	"net/http"
 )
 
 const (
 	apiKey             = "RGAPI-faecaef7-51c3-401f-902a-1072e311d77d"
-	championV3Url      = "https://euw1.api.riotgames.com/lol/platform/v3/champions?freeToPlay=false"
-	lolStaticDataV3Url = "https://euw1.api.riotgames.com/lol/static-data/v3/champions/1?locale=en_GB"
+	championV3Url      = "https://euw1.api.riotgames.com/lol/platform/v3/champions"
+	lolStaticDataV3Url = "https://euw1.api.riotgames.com/lol/static-data/v3/champions"
 )
 
-type lolStaticV3Response struct {
+type lolStaticJSON struct {
 	Title string `json:"title"`
 	Name  string `json:"name"`
 	Key   string `json:"key"`
 	ID    int    `json:"id"`
 }
 
-type championV3Response struct {
-	Champion []struct {
-		RankedPlayEnabled bool `json:"rankedPlayEnabled"`
-		BotEnabled        bool `json:"botEnabled"`
-		BotMmEnabled      bool `json:"botMmEnabled"`
-		Active            bool `json:"active"`
-		FreeToPlay        bool `json:"freeToPlay"`
-		ID                int  `json:"id"`
-	} `json:"champions"`
+type championJSON struct {
+	Champions []championJSONInner `json:"champions"`
+}
+
+type championJSONInner struct {
+	RankedPlayEnabled bool `json:"rankedPlayEnabled"`
+	BotEnabled        bool `json:"botEnabled"`
+	BotMmEnabled      bool `json:"botMmEnabled"`
+	Active            bool `json:"active"`
+	FreeToPlay        bool `json:"freeToPlay"`
+	ID                int  `json:"id"`
+}
+
+type championInfo struct {
+	ID   int
+	Name string
 }
 
 func main() {
-
-	body := genericAPICall(lolStaticDataV3Url, &lolStaticV3Response{})
-	a := lolStaticV3Response{}
-	err = json.Unmarshal(body, &a)
-	check(err)
-
+	ids := getChampionIDs()
+	champions := getChampionInfo(ids...)
+	fmt.Println(champions)
 }
 
-func genericAPICall(baseURL string) *[]byte {
-	url := baseURL + "&api_key=" + apiKey
+func getChampionIDs() []int {
+	c := championJSON{}
+	getJSON(championV3Url, &c)
+	var ids []int
+	for _, v := range c.Champions {
+		ids = append(ids, v.ID)
+	}
+	return ids
+}
+
+func getChampionInfo(ids ...int) []championInfo {
+	var c []championInfo
+	for _, id := range ids {
+		url := fmt.Sprintf("%s%s%d", lolStaticDataV3Url, "/", id)
+		b := lolStaticJSON{}
+		getJSON(url, &b)
+		c = append(c, championInfo{ID: id, Name: b.Name})
+		fmt.Println(id, b.Name)
+	}
+	return c
+}
+
+func getJSON(baseURL string, target interface{}) error {
+	url := baseURL + "?api_key=" + apiKey
 	res, err := http.Get(url)
 	check(err)
-
-	body, err := ioutil.ReadAll(res.Body)
-	check(err)
 	defer res.Body.Close()
-
-	return &body
+	return json.NewDecoder(res.Body).Decode(target)
 }
 
-// check is a helper error function
 func check(e error) {
 	if e != nil {
 		panic(e)
